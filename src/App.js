8fd3a7c77ba301ed547e3ff3289b59c6c8864887
shivika24 <43493690+shivika24@users.noreply.github.com>
2020-06-28 16:46:34 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -39,69 +39,69 @@ function App() {
         <input type="text" style={{width:"100%",height:"40px",height:"100%"}} ref={inputRef} value={result}/>
       </div>
       <div className="row" style={{padding:0,margin:0,width:"90%",height:"40px"}}>
-        <div className="col-md-6" style={{padding:0,margin:0}}>
+        <div className="col-md-6 col-12" style={{padding:0,margin:0}}>
           <button className="btn1" style={{width:"100%",height:"40px"}} onClick={onClear}>Clear</button>
         </div>
-        <div className="col-md-3" style={{padding:0,margin:0}} onClick={backspace}>
+        <div className="col-md-3 col-12" style={{padding:0,margin:0}} onClick={backspace}>
         <button className="btn1" style={{width:"100%",height:"40px"}}>C</button>
         </div>
-        <div className="col-md-3" style={{padding:0,margin:0}}>
+        <div className="col-md-3 col-12" style={{padding:0,margin:0}}>
         <button className="btn2" style={{width:"100%",height:"40px"}} name="+" onClick={handleClick}>+</button>
         </div>
       </div>
       <div className="row" style={{padding:0,margin:0,width:"90%",height:"40px"}}>
-        <div className="col-md-3" style={{padding:0,margin:0}}>
+        <div className="col-md-3 col-12" style={{padding:0,margin:0}}>
           <button className="btn1" style={{width:"100%",height:"40px"}} name="7" onClick={handleClick}>7</button>
         </div>
-        <div className="col-md-3" style={{padding:0,margin:0}}>
+        <div className="col-md-3 col-12" style={{padding:0,margin:0}}>
           <button className="btn1" style={{width:"100%",height:"40px"}} name="8" onClick={handleClick}>8</button>
         </div>
-        <div className="col-md-3" style={{padding:0,margin:0}}>
+        <div className="col-md-3 col-12" style={{padding:0,margin:0}}>
         <button className="btn1" style={{width:"100%",height:"40px"}} name="9" onClick={handleClick}>9</button>
         </div>
-        <div className="col-md-3" style={{padding:0,margin:0}}>
+        <div className="col-md-3 col-12" style={{padding:0,margin:0}}>
         <button className="btn2" style={{width:"100%",height:"40px"}} name="-" onClick={handleClick}>-</button>
         </div>
       </div> 
       <div className="row" style={{padding:0,margin:0,width:"90%",height:"40px"}}>
-        <div className="col-md-3" style={{padding:0,margin:0}}>
+        <div className="col-md-3 col-12" style={{padding:0,margin:0}}>
           <button className="btn1" style={{width:"100%",height:"40px"}} name="4" onClick={handleClick}>4</button>
         </div>
-        <div className="col-md-3" style={{padding:0,margin:0}}>
+        <div className="col-md-3 col-12" style={{padding:0,margin:0}}>
           <button className="btn1" style={{width:"100%",height:"40px"}} name="5" onClick={handleClick}>5</button>
         </div>
-        <div className="col-md-3" style={{padding:0,margin:0}}>
+        <div className="col-md-3 col-12" style={{padding:0,margin:0}}>
         <button className="btn1" style={{width:"100%",height:"40px"}} name="6" onClick={handleClick}>6</button>
         </div>
-        <div className="col-md-3" style={{padding:0,margin:0}}>
+        <div className="col-md-3 col-12" style={{padding:0,margin:0}}>
         <button className="btn2" style={{width:"100%",height:"40px"}} name="*" onClick={handleClick}>*</button>
         </div>
       </div>   
       <div className="row" style={{padding:0,margin:0,width:"90%",height:"40px"}}>
-        <div className="col-md-3" style={{padding:0,margin:0}}>
+        <div className="col-md-3 col-12" style={{padding:0,margin:0}}>
           <button className="btn1" style={{width:"100%",height:"40px"}} name="1" onClick={handleClick}>1</button>
         </div>
-        <div className="col-md-3" style={{padding:0,margin:0}}>
+        <div className="col-md-3 col-12" style={{padding:0,margin:0}}>
           <button className="btn1" style={{width:"100%",height:"40px"}} name="2" onClick={handleClick}>2</button>
         </div>
-        <div className="col-md-3" style={{padding:0,margin:0}}>
+        <div className="col-md-3 col-12" style={{padding:0,margin:0}}>
         <button className="btn1" style={{width:"100%",height:"40px"}} name="3" onClick={handleClick}>3</button>
         </div>
-        <div className="col-md-3" style={{padding:0,margin:0}}>
+        <div className="col-md-3 col-12" style={{padding:0,margin:0}}>
         <button className="btn2" style={{width:"100%",height:"40px"}} name="/" onClick={handleClick}>/</button>
         </div>
       </div>    
       <div className="row" style={{padding:0,margin:0,width:"90%",height:"40px"}}>
-        <div className="col-md-3" style={{padding:0,margin:0}}>
+        <div className="col-md-3 col-12" style={{padding:0,margin:0}}>
           <button className="btn1" style={{width:"100%",height:"40px"}} name="0" onClick={handleClick}>0</button>
         </div>
-        <div className="col-md-3" style={{padding:0,margin:0}}>
+        <div className="col-md-3 col-12" style={{padding:0,margin:0}}>
         <button className="btn1" style={{width:"100%",height:"40px"}} name="." onClick={handleClick}>.</button>
         </div>
-        <div className="col-md-3" style={{padding:0,margin:0}}>
+        <div className="col-md-3 col-12" style={{padding:0,margin:0}}>
         <button className="btn1" style={{width:"100%",height:"40px"}} name="%" onClick={handleClick}>%</button>
         </div>
-        <div className="col-md-3" style={{padding:0,margin:0}}>
+        <div className="col-md-3 col-12" style={{padding:0,margin:0}}>
         <button className="btn2" style={{width:"100%",height:"40px"}} onClick={calculate}>=</button>
         </div>
       </div> 
